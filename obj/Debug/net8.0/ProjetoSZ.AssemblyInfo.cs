6a25f22fa7d922d7d33a64b2b77ddf2ab95b43c6
Renan Gomes <renan.gmj87@gmail.com>
2024-08-26 09:44:35 -0300

    Carousel test

--- obj/Debug/net8.0/ProjetoSZ.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoSZ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoSZ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd4639d94cfa70c714cdbf5617c3826312008c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0d294ce1c1df9f83a2f09492aed93dc569492c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoSZ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoSZ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
